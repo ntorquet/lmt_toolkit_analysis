ba9965b8ad9031f392b4a741ffe46c38d8b48ea5
Nicolas Torquet <42234278+ntorquet@users.noreply.github.com>
2025-06-04 10:58:54 +0200

    activityPreset.vue modified in Composition API. #90

--- nuxt-api/components/activityPreset.vue
+++ nuxt-api/components/activityPreset.vue
@@ -7,6 +7,20 @@ PHENOMIN, CNRS UMR7104, INSERM U964, Université de Strasbourg
 Code under GPL v3.0 licence
 -->
 
+<script setup>
+////////////////////////////////
+// PROPS
+////////////////////////////////
+const props = defineProps({
+  timeBin: {
+    type: Number,
+    required: true,
+  }
+});
+
+
+</script>
+
 <template>
   <v-card>
       <v-card-title> Activity preset</v-card-title>
@@ -16,31 +30,15 @@ Code under GPL v3.0 licence
             By default, the analysis will be done on the total duration of the experiment.<br />
             You have to select a time bin to proceed.
           </v-alert>
-          <v-text-field label="Time bin in minutes" v-model="timeBinComponent"></v-text-field>
+          <v-text-field label="Time bin in minutes" v-model="props.timeBin"></v-text-field>
 
-          <v-btn v-if="timeBinComponent!=''" @click="doAnalysis('activityPreset')">
+          <v-btn v-if="props.timeBin!=''" @click="doAnalysis('activityPreset')">
               <v-icon icon="mdi-arrow-right-bold"></v-icon>  Analyse
           </v-btn>
       </v-card-text>
   </v-card>
 </template>
 
-<script>
-export default {
-  name: "ActivityPreset",
-  props: {
-    timeBin: Number
-  },
-  data() {
-    return {
-      timeBinComponent: Number,
-    }
-  },
-  mounted() {
-    this.timeBinComponent = this.timeBin
-  }
-}
-</script>
 
 <style scoped>
 
