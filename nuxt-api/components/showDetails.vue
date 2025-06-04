4f68803d17daff1a3984d782108a4c10c48f90a5
Nicolas Torquet <42234278+ntorquet@users.noreply.github.com>
2025-06-04 10:23:33 +0200

    index.vue and showDetails.vue modified in Composition API. Details updated to give information about steps. #90

--- nuxt-api/components/showDetails.vue
+++ nuxt-api/components/showDetails.vue
@@ -6,34 +6,33 @@ CNRS - Mouse Clinical Institute
 PHENOMIN, CNRS UMR7104, INSERM U964, Université de Strasbourg
 Code under GPL v3.0 licence
 -->
+
+<script setup>
+////////////////////////////////
+// PROPS
+////////////////////////////////
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true,
+  }
+});
+
+</script>
+
 <template>
   <v-dialog
     width="800"
   >
     <v-card>
-      <v-card-title><v-icon :icon="data.icon" :color="data.color"></v-icon> {{ data.title }}</v-card-title>
-      <v-divider :color="data.color" thickness="2" class="border-opacity-100"></v-divider>
+      <v-card-title><v-icon :icon="props.data.icon" :color="props.data.color"></v-icon> {{ props.data.title }}</v-card-title>
+      <v-divider :color="props.data.color" thickness="2" class="border-opacity-100"></v-divider>
       <v-card-text>
-        {{ data.text }}
+        {{ props.data.text }}
       </v-card-text>
     </v-card>
   </v-dialog>
 </template>
-
-<script>
-export default {
-  name: "showDetails",
-  props: {
-    data: Object,
-    // showDetailsInModal: Boolean
-  },
-  data () {
-    return {
-      dialog: false,
-    }
-  },
-}
-</script>
 
 <style scoped>
 
