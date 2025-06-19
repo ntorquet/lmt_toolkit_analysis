e4b7691857ad9c48e4ab5e0b685c2fcbeff49ca9
Nicolas Torquet <42234278+ntorquet@users.noreply.github.com>
2025-06-19 14:44:29 +0200

    New component presetInformation.vue to explain what the preset will do #103 .

---
+++ nuxt-api/components/presetInformation.vue
@@ -0,0 +1,42 @@
+<!--
+Created by Nicolas Torquet at 19/06/2025
+torquetn@igbmc.fr
+Copyright: CNRS - INSERM - UNISTRA - ICS - IGBMC
+CNRS - Mouse Clinical Institute
+PHENOMIN, CNRS UMR7104, INSERM U964, Université de Strasbourg
+Code under GPL v3.0 licence
+-->
+
+<script setup>
+////////////////////////////////
+// PROPS
+////////////////////////////////
+import {text} from "node:stream/consumers";
+
+const props = defineProps({
+  presetName: {
+    type: String,
+    required: true
+  },
+  presetDescription: {
+    type: text,
+    required: true
+  }
+});
+</script>
+
+
+<template>
+  <v-card>
+    <v-card-title>{{ props.presetName }}</v-card-title>
+    <v-card-text>{{ props.presetDescription }}</v-card-text>
+  </v-card>
+</template>
+
+
+
+
+
+<style scoped>
+
+</style>
